fe7d36491bc0fa4504e7d47e9e502b7cd9c77303
Edwardrerk <edwardrerk@gmail.com>
2023-04-05 08:29:08 -0700

    Update route53.tf

--- Project-ELK-AWS/route53.tf
+++ Project-ELK-AWS/route53.tf
@@ -1,11 +1,11 @@
 # Manage a Route53 hosted zone
 resource "aws_route53_zone" "ererkt" {
-  name = "zztalks.xyz"
+  name = "rerktserver.com"
 }
 # Provide a Route53 record resource
 resource "aws_route53_record" "elk" {
   zone_id = aws_route53_zone.ererkt.zone_id
-  name = "elk.zztalks.xyz"
+  name = "elk.rerktserver.com"
   type = "A"
   ttl = "300"
   records = [aws_eip.ip.public_ip]
